18182cf8e0366605c629dde5f996653e420d93e3
Danish Ali <danish@codendevs.com>
2020-12-02 13:00:57 +0500

    added SweetAlerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
+  
   declarations: [
     AppComponent,
     ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SweetAlert2Module.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,13 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
+  @ViewChild('emptySwal') private emptySwal!: SwalComponent;
+  list:any[] = new Array();
+  val:string = "";
+  duplicate:number = 0;
+  
   constructor() { }
+
+  onEnterKey(event:any){
+    this.val = event.target.value;
+    debugger;
+    if(this.val == ""){
+      //alert("value is empty");
+      this.emptySwal.fire();
+    }
+    else
+    {
+      this.list.forEach(element => {
+        if(element.text == this.val){
+          this.duplicate++;
+        }
+      });
+      
+      if(this.duplicate < 1){
+        this.list.push({"text" : this.val, "isChecked" : false});
+      }
+      event.target.value = "";
+    }
+  }
+  
+  onAddBtnClick(newitem:any){
+    this.val = newitem.value;
+    if(this.val == ""){
+      //alert("value is empty");
+      this.emptySwal.fire();
+    }
+    else
+    {
+      this.list.forEach(element => {
+        if(element.text == this.val){
+          this.duplicate++;
+        }
+      });
+
+      if(this.duplicate < 1){
+        this.list.push({"text" : this.val, "isChecked" : false});
+      }
+      newitem.value = "";
+    }
+  }
 
   ngOnInit(): void {
   }
